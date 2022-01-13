d72b1b77f4ba8d9c7ce21223c8b41c894c7e36af
palashcodez <82249617+palashcodez@users.noreply.github.com>
2022-01-12 18:47:41 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-song="";
+song1="";
+song2="";
+lwristx=0;
+lwristy=0;
+rwristx=0;
+rwristy=0;
 
 function setup(){
     canvas=createCanvas(600, 500);
     canvas.position(665, 350);
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("MODeeAL AAIS LOEEDED");
 }
 
 function preload(){
@@ -27,3 +38,18 @@ function pause(){
     song.pause();
 }
 
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        lwristx=results[0].pose.leftWrist.x;
+        lwristy=results[0].pose.leftWrist.y;
+        rwristx=results[0].pose.rightWrist.x;
+        rwristy=results[0].pose.rightWrist.y;
+
+        console.log(lwristx+'lwx');
+        console.log(lwristy+'lwy');
+        console.log(rwristx+'rwx');
+        console.log(rwristy+'rwy');
+    }
+    
+}
